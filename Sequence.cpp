9c777c08baa7c42e9de8cefb319c1c6c433dbca3
AverageComputerNerd <130625952+AverageComputerNerd@users.noreply.github.com>
2023-04-25 22:29:16 -0700

    Update Sequence.cpp

--- Sequence.cpp
+++ Sequence.cpp
@@ -1,108 +1,338 @@
-// Sequence.cpp
-
 #include "Sequence.h"
 
+#include <iostream>
+using namespace std;
+
 Sequence::Sequence()
- : m_size(0)
-{}
-
-int Sequence::insert(int pos, const ItemType& value)
 {
-    if (pos < 0  ||  pos > size()  ||  size() == DEFAULT_MAX_ITEMS)
-    return -1;
-    uncheckedInsert(pos, value);
-    return pos;
+    head = nullptr;
+    tail = nullptr;
+    m_size = 0;
 }
 
-int Sequence::insert(const ItemType& value)
+Sequence::Sequence(const Sequence& other)
 {
-    if (size() == DEFAULT_MAX_ITEMS)
-        return -1;
-    int pos;
-    for (pos = 0; pos < size()  &&  value > m_data[pos]; pos++)
-    ;
-    uncheckedInsert(pos, value);
-    return pos;
+    if(other.head == nullptr){
+        head = nullptr;
+        tail = nullptr;
+        m_size = 0;
+        return;
+    }
+    
+    head = nullptr;
+    tail = nullptr;
+    m_size = 0;
+    for(int i=0; i<other.size(); i++)
+    {
+        ItemType t;
+        other.get(i, t);
+        insert(i, t);
+    }
 }
-     
-bool Sequence::erase(int pos)
+
+Sequence& Sequence::operator=(const Sequence& rhs)
 {
-    if (pos < 0  ||  pos >= size())
+    if (this != &rhs)
+    {
+        Sequence temp(rhs);
+        swap(temp);
+    }
+    return *this;
+}
+
+Sequence::~Sequence(){
+    Node* p = head;
+    while (p != nullptr){
+        Node* next = p->next;
+        delete p;
+        p = next; }
+    
+    head = nullptr;
+    tail = nullptr;
+    m_size = 0;
+}
+
+bool Sequence::empty() const
+{
+    if (head==nullptr && tail==nullptr && m_size == 0){ //simply check if theres nothing there
+        return true;
+    }
     return false;
-    for ( ; pos < size() - 1; pos++)
-    m_data[pos] = m_data[pos+1];
+}
+
+int Sequence::size() const
+{
+    return m_size;
+}
+
+int Sequence::insert (int pos, const ItemType& value)
+{
+    if(pos > m_size || pos < 0)
+    {
+        return -1;
+    }
+    
+    if( pos == 0 && head == nullptr) // initialize a new DLL
+    {
+        Node* newNode = new Node;
+        newNode->data = value;
+        head = newNode;
+        tail = newNode;
+        
+        newNode->next = nullptr;
+        newNode->previous = nullptr;
+    }
+    else if (pos == m_size) // insert at the end of a DLL
+    {
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode->next = nullptr;
+//        Node* move = head;
+//        while (move->next != nullptr)
+//        {
+//            move = move->next;
+//        }
+        newNode->previous = tail;
+        tail->next = newNode;
+        tail = newNode;
+    }
+    else if(pos == 0 && head != nullptr) // insert at the beginging of a DLL
+    {
+        Node* newNode = new Node;
+        newNode->data = value;
+        
+        newNode->next = head;
+        newNode->previous = nullptr;
+        
+        head->previous = newNode;
+        head = newNode;
+    }
+    else if (pos > 0 && pos < m_size) // insert inside the DLL
+    {
+        Node* newNode = new Node;
+        newNode->data = value;
+        
+        int count = 0;
+        Node* move = head;
+        while (count != pos) //loop until move is at the pos
+        {
+            move = move->next;
+            count++;
+        }
+        newNode->next = move;
+        newNode->previous = move->previous;
+        move->previous->next = newNode;
+        move->previous = newNode;
+    }
+    m_size++;
+    return pos;
+}
+
+int Sequence::insert (const ItemType& value)
+{
+    Node* toMove = head;
+    int pos = 0;
+
+    //if empty list add at the begining
+    if (head == nullptr && m_size == 0)
+    {
+        return (insert(0, value));
+    }
+    //if list is not empty loop through and find the first instance where value <= an item
+    while(toMove != nullptr)
+    {
+        if(value <= toMove->data)
+        {
+            break;
+        }
+        pos++;
+        toMove = toMove->next;
+    }
+
+    insert(pos, value);
+    return pos;
+}
+
+bool Sequence::erase (int pos)
+{
+    if(head == nullptr || tail == nullptr || pos >= m_size || pos < 0 || m_size == 0){
+        return false;
+    }
+    
+    //first checking extrenous scenarios
+    
+    if(pos==0&&m_size!= 0){  //accounting for head when null
+        Node* tmp = head;
+        head = head->next;
+        head->previous = nullptr;
+        delete tmp;
+        m_size--;
+        return true;
+    }
+    if(pos==0 && m_size==1)  //accunting for 1 element
+    {
+        Node* tmp = head;
+        head = nullptr;
+        tail = nullptr;
+        delete tmp;
+        m_size--;
+        return true;
+    }
+    if(pos == 0 && m_size != 0){  //normal middle part
+        Node* tmp = head;
+        head = head->next;
+        head->previous = nullptr;
+        delete tmp;
+        m_size--;
+        return true;
+    }
+    if(pos == (m_size - 1) && m_size != 0){ // deleting the tail ptr
+        Node* tmp = tail;
+        tail = tail->previous;
+        tail->next = nullptr;
+        delete tmp;
+        m_size--;
+        return true;
+    }
+    
+    
+    Node* toMove = head;
+    int count = 0;
+    while (count!=pos){
+        toMove = toMove->next;
+        count++;
+    }
+    toMove->previous->next = toMove->next;
+    toMove->next->previous = toMove->previous;
+    delete toMove;
     m_size--;
     return true;
 }
 
 int Sequence::remove(const ItemType& value)
 {
-    int keepPos = find(value);
-    if (keepPos == -1)
-    return 0;
-    int count = 1;
-    for (int examinePos = keepPos+1; examinePos < size(); examinePos++)
-    {
-    if (m_data[examinePos] == value)
-        count++;
-    else
-    {
-        m_data[keepPos] = m_data[examinePos];
-        keepPos++;
+    if (head == nullptr){
+        return 0;}
+    
+    Node *p = head;
+    int pos = 0;
+    int count = 0;
+    while (p != nullptr){
+        if (p->data == value){
+            p = p->next;
+            erase(pos);
+            count++;
+            pos--;
+        }
+        else{
+            p = p->next;
+            pos++;}
     }
-    }
-    m_size -= count;
     return count;
 }
-     
-bool Sequence::get(int pos, ItemType& value) const
+
+bool Sequence::get (int pos, ItemType& value) const
 {
-    if (pos < 0  ||  pos >= size())
+    if (pos<0||pos>= m_size){
         return false;
-    value = m_data[pos];
+    }
+    Node* toMove = head;
+    for (int i = 0; i != pos; i++){
+        toMove = toMove->next;
+    }
+    value = toMove->data;
     return true;
 }
 
-bool Sequence::set(int pos, const ItemType& value)
+bool Sequence::set (int pos, const ItemType& value)
 {
-    if (pos < 0  ||  pos >= size())
+    if (head == nullptr||pos<0||pos>=m_size){
         return false;
-    m_data[pos] = value;
+    }
+    Node* toMove = head;
+    int count = 0;
+    while (count!=pos){
+        toMove = toMove->next;
+        count++;
+    }
+    toMove->data = value;
     return true;
 }
+
 
 int Sequence::find(const ItemType& value) const
 {
-    for (int pos = 0; pos < size(); pos++)
-        if (m_data[pos] == value)
-        return pos;
+    Node *p;
+    int x = 0;
+    for (p=head; p!=nullptr; p=p->next)
+    {
+        if (p->data == value)
+            return x;
+        x++;
+    }
     return -1;
 }
 
-void Sequence::swap(Sequence& other)
+void Sequence::swap (Sequence& other)
 {
-      // Swap elements.  Since the only elements that matter are those up to
-      // m_size and other.m_size, only they have to be moved.
-
-    int maxSize = (m_size > other.m_size ? m_size : other.m_size);
-    for (int k = 0; k < maxSize; k++)
-    {
-        ItemType tempItem = m_data[k];
-        m_data[k] = other.m_data[k];
-        other.m_data[k] = tempItem;
-    }
-
-      // Swap sizes
+    Node* tmpH = head;
+    head = other.head;
+    other.head = tmpH;
+    
+    Node* tempH = tail;
+    tail = other.tail;
+    other.tail = tempH;
 
     int tempSize = m_size;
     m_size = other.m_size;
     other.m_size = tempSize;
 }
 
-void Sequence::uncheckedInsert(int pos, const ItemType& value)
+
+int subsequence(const Sequence& seq1, const Sequence& seq2)
 {
-    for (int k = size(); k > pos; k--)
-    m_data[k] = m_data[k-1];
-    m_data[pos] = value;
-    m_size++;
+    if(seq1.empty() || seq2.empty()|| seq2.size() > seq1.size())
+    {
+        return -1;
+    }
+    ItemType x;
+    ItemType y;
+    for(int i = 0; i < seq1.size(); i++){
+        if ((seq1.size() - seq2.size() - i) < 0){
+            break;
+        }
+        seq1.get(i,x);
+        seq2.get(0,y);
+        if(x==y){
+            for(int j = 0; j<seq2.size(); j++){
+                seq1.get(i+j,x);
+                seq2.get(j,y);
+
+                if(x!=y){
+                    break;
+                }
+                else if(j==seq2.size() - 1){
+                    return i;} //returning index of where the match occured
+
+            } //for loop inner
+        } // if
+    } //for loop outer
+    return -1;  //all else fails, return -1
+
 }
+
+void concatReverse(const Sequence& seq1, const Sequence& seq2, Sequence& result) {
+    /*Sequence tmp;
+
+    for (int i = seq1.size() - 1; i >= 0; i--) {
+        tmp.insert(seq1[i]);
+    }
+
+    for (int i = seq2.size() - 1; i >= 0; i--) {
+        tmp.insert(seq2[i]);
+    }
+
+    result = tmp; */
+}
+
+
